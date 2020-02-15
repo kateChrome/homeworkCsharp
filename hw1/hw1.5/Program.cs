88e6f721053773dc983100344b80bb13970e5994
kat3chrome <shandkat3@gmail.com>
2020-02-15 19:34:12 +0300

    ready program

--- hw1/hw1.5/Program.cs
+++ hw1/hw1.5/Program.cs
@@ -15,19 +15,41 @@ namespace hw1._5
             Console.Write("Enter the number of size of line: ");
             int sizeOfLine = int.Parse(Console.ReadLine());
 
-            int[][] inputArray = new int[sizeOfLine][];
+            int[,] inputArray = new int[numberOflines, sizeOfLine];
+            int[] temporaryArray = new int[sizeOfLine];
+
+            Console.WriteLine("Enter matrix line by line:");
             for (int i = 0; i < numberOflines; i++)
             {
-                inputArray[i] = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                temporaryArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int j = 0; j < sizeOfLine; j++)
+                {
+                    inputArray[i, j] = temporaryArray[j];
+                }
             }
 
-            Array.Sort(inputArray);
+            for (int i = 0; i < sizeOfLine; i++)
+            {
+                for (int j = 0; j < sizeOfLine - 1; j++)
+                {
+                    if (inputArray[0, j] < inputArray[0, j + 1])
+                    {
+                        for (int k = 0; k < numberOflines; k++)
+                        {
+                            int temporaryVariable = inputArray[k, j];
+                            inputArray[k, j] = inputArray[k, j + 1];
+                            inputArray[k, j + 1] = temporaryVariable;
+                        }
+                    }
+                }
+            }
 
+            Console.WriteLine("Sorted matrix:");
             for (int i = 0; i < numberOflines; i++)
             {
                 for (int j = 0; j < sizeOfLine; j++)
                 {
-                    Console.Write(inputArray[i][j] + " ");
+                    Console.Write(inputArray[i, j] + " ");
                 }
                 Console.Write(Environment.NewLine);
             }
