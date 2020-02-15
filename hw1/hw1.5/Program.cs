7b1927c11c31c3eb1c2a1f2b24cb9821eeff6a3f
kat3chrome <shandkat3@gmail.com>
2020-02-15 16:00:33 +0300

    add input

--- hw1/hw1.5/Program.cs
+++ hw1/hw1.5/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace hw1._5
 {
@@ -6,7 +10,28 @@ namespace hw1._5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter the number of lines: ");
+            int numberOflines = int.Parse(Console.ReadLine());
+            Console.Write("Enter the number of size of line: ");
+            int sizeOfLine = int.Parse(Console.ReadLine());
+
+            int[][] inputArray = new int[sizeOfLine][];
+            for (int i = 0; i < numberOflines; i++)
+            {
+                inputArray[i] = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            }
+
+            Array.Sort(inputArray);
+
+            for (int i = 0; i < numberOflines; i++)
+            {
+                for (int j = 0; j < sizeOfLine; j++)
+                {
+                    Console.Write(inputArray[i][j] + " ");
+                }
+                Console.Write(Environment.NewLine);
+            }
+
         }
     }
 }
