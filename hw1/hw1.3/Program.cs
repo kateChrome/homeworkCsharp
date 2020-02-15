03c10520ca92710354833748484c96f9c5009d9a
kat3chrome <shandkat3@gmail.com>
2020-02-15 13:31:46 +0300

    ready array sort

---
+++ hw1/hw1.3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class ArraySort
+{
+    public static void Main(string[] args)
+    {
+        int sizeOfArray;
+        Console.Write("Enter the number of elements: ");
+        sizeOfArray = int.Parse(Console.ReadLine());
+        Console.Write("Enter values of array: ");
+        var inputArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        
+        Array.Sort(inputArray);
+        Console.Write("Sorted array: ");
+        Console.WriteLine(String.Join(" ", inputArray));
+    }
+}
