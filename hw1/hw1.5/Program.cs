a7eda5c3bbe295ad00d76b3d187952f6e7e8955e
kat3chrome <shandkat3@gmail.com>
2020-02-15 14:56:18 +0300

    add files

---
+++ hw1/hw1.5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hw1._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
