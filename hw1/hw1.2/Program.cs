092291cc52cc9b90c47f6617e67437687cfd984f
kat3chrome <shandkat3@gmail.com>
2020-02-15 12:44:01 +0300

    ready Fibonacci program

---
+++ hw1/hw1.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class Fibonacci
+{
+    public static void Main(string[] args)
+    {
+        int firstValue = 1;
+        int secondValue = 1;
+        int temporaryValue, inputNumber;
+
+        Console.Write("Enter the number of elements: ");
+        inputNumber = int.Parse(Console.ReadLine());
+        Console.Write("Fibonacci values: " + firstValue + " " + secondValue + " ");
+        for (int i = 2; i < inputNumber; i++)
+        {
+            temporaryValue = firstValue + secondValue;
+            Console.Write(temporaryValue + " ");
+            firstValue = secondValue;
+            secondValue = temporaryValue;
+        }
+        
+        Console.Write(Environment.NewLine);
+    }
+}
