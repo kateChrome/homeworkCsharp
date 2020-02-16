ccca3d11d955e26ec8024946b1426ed649ec7548
kat3chrome <shandkat3@gmail.com>
2020-02-17 00:55:38 +0300

    add factorial realisation

--- hw1/hw1.1/Program.cs
+++ hw1/hw1.1/Program.cs
@@ -6,7 +6,17 @@ namespace hw1._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter value: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int result = 1;
+            while (number != 1)
+            {
+                result = result * number;
+                number = number - 1;
+            }
+
+            Console.WriteLine("Factorial of the number is: " + result);
         }
     }
 }
