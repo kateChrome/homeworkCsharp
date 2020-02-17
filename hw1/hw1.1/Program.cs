5ff4fd391b794d4c61d492a7cdbcaaefa488cbb2
kat3chrome <shandkat3@gmail.com>
2020-02-17 23:20:55 +0300

    split by classes

--- hw1/hw1.1/Program.cs
+++ hw1/hw1.1/Program.cs
@@ -1,22 +1,27 @@
 ﻿using System;
 
-namespace hw1._1
+namespace Hw1._1
 {
-    class Program
+    class Factorial
     {
+        static int computingFactorial(int value)
+        {
+            int result = 1;
+            while (value != 1)
+            {
+                result = result * value;
+                value--;
+            }
+            return result;
+        }
         static void Main(string[] args)
         {
             Console.Write("Enter value: ");
             int number = Convert.ToInt32(Console.ReadLine());
 
-            int result = 1;
-            while (number != 1)
-            {
-                result = result * number;
-                number = number - 1;
-            }
+            int result = computingFactorial(number);
 
-            Console.WriteLine("Factorial of the number is: " + result);
+            Console.WriteLine($"Factorial of the number is: {result}");
         }
     }
 }
