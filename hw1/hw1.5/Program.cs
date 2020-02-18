00fff92e8b55e6406ad1a68074bdbf1a62891ede
kat3chrome <shandkat3@gmail.com>
2020-02-18 19:17:31 +0300

    split by methods

--- hw1/hw1.5/Program.cs
+++ hw1/hw1.5/Program.cs
@@ -8,15 +8,28 @@ namespace hw1._5
 {
     class Program
     {
-        static int[,] sortMatrixByFirstElement(int[,] inputArray, int numberOflines, int sizeOfLine)
+        static void GetInputMatrix(int[,] inputArray)
         {
-            for (int i = 0; i < sizeOfLine; i++)
+            int[] temporaryArray = new int[inputArray.GetLength(1)];
+            for (int i = 0; i < inputArray.GetLength(0); i++)
             {
-                for (int j = 0; j < sizeOfLine - i - 1; j++)
+                temporaryArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int j = 0; j < inputArray.GetLength(1); j++)
+                {
+                    inputArray[i, j] = temporaryArray[j];
+                }
+            }
+        }
+
+        static void SortMatrixByFirstElement(int[,] inputArray)
+        {
+            for (int i = 0; i < inputArray.GetLength(1); i++)
+            {
+                for (int j = 0; j < inputArray.GetLength(1) - i - 1; j++)
                 {
                     if (inputArray[0, j] < inputArray[0, j + 1])
                     {
-                        for (int k = 0; k < numberOflines; k++)
+                        for (int k = 0; k < inputArray.GetLength(0); k++)
                         {
                             int temporaryVariable = inputArray[k, j];
                             inputArray[k, j] = inputArray[k, j + 1];
@@ -25,41 +38,33 @@ namespace hw1._5
                     }
                 }
             }
-
-            return inputArray;
         }
 
+        static void PrintMatrix(int[,] inputArray)
+        {
+            for (int i = 0; i < inputArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < inputArray.GetLength(1); j++)
+                {
+                    Console.Write($"{inputArray[i, j]} ");
+                }
+                Console.Write(Environment.NewLine);
+            }
+        }
         static void Main(string[] args)
         {
             Console.Write("Enter the number of lines: ");
             int numberOflines = int.Parse(Console.ReadLine());
             Console.Write("Enter the size of line: ");
             int sizeOfLine = int.Parse(Console.ReadLine());
-
             int[,] inputArray = new int[numberOflines, sizeOfLine];
-            int[] temporaryArray = new int[sizeOfLine];
 
             Console.WriteLine("Enter matrix line by line:");
-            for (int i = 0; i < numberOflines; i++)
-            {
-                temporaryArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
-                for (int j = 0; j < sizeOfLine; j++)
-                {
-                    inputArray[i, j] = temporaryArray[j];
-                }
-            }
-
-            inputArray = sortMatrixByFirstElement(inputArray, numberOflines, sizeOfLine);
+            GetInputMatrix(inputArray);
+            SortMatrixByFirstElement(inputArray);
 
             Console.WriteLine("Sorted matrix:");
-            for (int i = 0; i < numberOflines; i++)
-            {
-                for (int j = 0; j < sizeOfLine; j++)
-                {
-                    Console.Write($"{inputArray[i, j]} ");
-                }
-                Console.Write(Environment.NewLine);
-            }
+            PrintMatrix(inputArray);
 
         }
     }
