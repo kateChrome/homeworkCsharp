bfa064caf96ed347c50198e9e8dea0eb343b8ba2
kat3chrome <shandkat3@gmail.com>
2020-02-18 17:52:30 +0300

    fix

--- hw1/hw1.1/Program.cs
+++ hw1/hw1.1/Program.cs
@@ -4,7 +4,7 @@ namespace Hw1._1
 {
     class Factorial
     {
-        static int computingFactorial(int value)
+        static int ComputeFactorial(int value)
         {
             int result = 1;
             while (value != 1)
@@ -14,12 +14,13 @@ namespace Hw1._1
             }
             return result;
         }
+        
         static void Main(string[] args)
         {
             Console.Write("Enter value: ");
             int number = Convert.ToInt32(Console.ReadLine());
 
-            int result = computingFactorial(number);
+            int result = ComputeFactorial(number);
 
             Console.WriteLine($"Factorial of the number is: {result}");
         }
