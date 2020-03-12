c18129426c07cc88871eb287730493ae05d60598
test <shandkat3@gmail.com>
2020-03-12 22:54:43 +0300

    fix space problem

--- hw4/hw4.1/src/Program.cs
+++ hw4/hw4.1/src/Program.cs
@@ -6,7 +6,8 @@ namespace src
     {
         static void Main(string[] args)
         {
-            string expression = "(* (+ 1 10) (- 3 (/ 10 5)))";
+            // string expression = "(* (+ 1 10) (- 3 (/ 10 5)))";
+            string expression = "(+ (/ (* (+ 1 10) (- 3 (/ 10 5))) 11) (- 5 3))";
             Tree tree = new Tree();
             tree.MakeTree(expression);
             var result = tree.CalculateTree();
