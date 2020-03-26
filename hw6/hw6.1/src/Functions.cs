9c05e4c62bf754680748e957b105dd476974a1cb
kateChrome <shandkat3@gmail.com>
2020-03-27 00:46:13 +0300

    add xml comments

--- hw6/hw6.1/src/Functions.cs
+++ hw6/hw6.1/src/Functions.cs
@@ -3,8 +3,17 @@ using System.Collections.Generic;
 
 namespace src
 {
+    /// <summary>
+    /// Class with implementation list functions.
+    /// </summary>
     public class Functions
     {
+        /// <summary>
+        /// Map list function.
+        /// </summary>
+        /// <param name="inputList">List for mapping.</param>
+        /// <param name="function">Function of mapping.</param>
+        /// <returns>Mapped list.</returns>
         public static List<int> Map(List<int> inputList, Func<int, int> function)
         {
             var outputList = new List<int>();
@@ -17,6 +26,12 @@ namespace src
             return outputList;
         }
 
+        /// <summary>
+        /// Filter list function.
+        /// </summary>
+        /// <param name="inputList">List for filtering.</param>
+        /// <param name="function">Function of filtering.</param>
+        /// <returns>List after filtered.</returns>
         public static List<int> Filter(List<int> inputList, Func<int, bool> function)
         {
             var outputList = new List<int>();
@@ -32,6 +47,13 @@ namespace src
             return outputList;
         }
 
+        /// <summary>
+        /// Fold list function.
+        /// </summary>
+        /// <param name="inputList">List for folding.</param>
+        /// <param name="accumulator">accumulator.</param>
+        /// <param name="function">Function of folding.</param>
+        /// <returns>accumulator after folding.</returns>
         public static int Fold(List<int> inputList, int accumulator, Func<int, int, int> function)
         {
             foreach (var item in inputList)
