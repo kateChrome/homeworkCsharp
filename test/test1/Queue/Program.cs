414e8d30c5cbf34a7edb87bfe3f82f059f9158e2
kateChrome <shandkat3@gmail.com>
2020-04-17 16:52:46 +0300

    add some tests

--- test/test1/Queue/Program.cs
+++ test/test1/Queue/Program.cs
@@ -5,19 +5,19 @@ namespace Queue
     {
         static void Main(string[] args)
         {
-            Queue<string> q = new Queue<string>("8 0", 8);
-            q.Enqueue("9 1", 9);
-            q.Enqueue("8 2", 8);
-            q.Enqueue("8 3", 8);
-            q.Enqueue("6 4", 6);
-            Console.WriteLine(q.Dequeue());
-            Console.WriteLine(q.Dequeue());
-            Console.WriteLine(q.Dequeue());
-            Console.WriteLine(q.Dequeue());
-            Console.WriteLine(q.Dequeue());
-            Console.WriteLine(q.Dequeue());
-            Console.WriteLine(q.Dequeue());
-            Console.WriteLine(q.Dequeue());
+            Queue<string> queue = new Queue<string>("8 0", 8);
+            queue.Enqueue("9 1", 9);
+            queue.Enqueue("8 2", 8);
+            queue.Enqueue("8 3", 8);
+            queue.Enqueue("6 4", 6);
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
         }
     }
 }
--- test/test1/Tests/UnitTest1.cs
+++ test/test1/Tests/UnitTest1.cs
@@ -1,18 +1,61 @@
+using System;
+using System.Collections;
 using NUnit.Framework;
 
-namespace Tests
+namespace Queue
 {
     public class Tests
     {
+
+        private Queue<int> queue;
+
         [SetUp]
         public void Setup()
         {
+            queue = new Queue<int>(0, 0);
         }
 
         [Test]
-        public void Test1()
+        public void TestQueueAddOneElement()
         {
-            Assert.Pass();
+            queue.Enqueue(1, 1);
+            Assert.AreEqual(queue.GetSize(), 2);
         }
+
+        [Test]
+        public void TestQueueAdd10Element()
+        {
+            for (int i = 0; i < 10; i ++)
+            {
+                queue.Enqueue(i, i);
+            }
+            Assert.AreEqual(queue.GetSize(), 11);
+        }
+
+        [Test]
+        public void TestDoubleGetFromSetupQueue()
+        {
+            try
+            {
+                queue.Dequeue();
+                queue.Dequeue();
+            }
+            catch (Exception exception)
+            {
+                Assert.AreEqual("Queue is empty", exception.Message);
+            }
+        }
+
+        [Test]
+        public void TestEnqueueAndDequeueCheckValue()
+        {
+            int element = 10;
+            queue.Enqueue(element, 19);
+            Assert.AreEqual(element, queue.Dequeue());
+        }
+
+        
+
+
     }
 }
