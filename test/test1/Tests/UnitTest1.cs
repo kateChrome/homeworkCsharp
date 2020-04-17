16b368ce7f0a8fa3aa8aca6710e4357062c46638
kateChrome <shandkat3@gmail.com>
2020-04-17 16:58:55 +0300

    add more tests

--- test/test1/Tests/UnitTest1.cs
+++ test/test1/Tests/UnitTest1.cs
@@ -54,8 +54,32 @@ namespace Queue
             Assert.AreEqual(element, queue.Dequeue());
         }
 
-        
+        [Test]
+        public void TestPriorityCheck()
+        {
+            for (int i = 10; i > 0; i--)
+            {
+                queue.Enqueue(i, 10 - i);
+            }
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();
 
+            Assert.AreEqual(6, queue.Dequeue());
+        }
 
+        [Test]
+        public void TestElementsWithEqualPriority()
+        {
+            queue.Enqueue(1, 1);
+            queue.Enqueue(10, 2);
+            queue.Enqueue(12, 2);
+            queue.Enqueue(14, 10);
+
+            queue.Dequeue();
+            Assert.AreEqual(10, queue.Dequeue());
+        }
     }
 }
