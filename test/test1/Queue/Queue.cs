f4218b45058467cfd60a56cfb1f4b43506dc9c93
kateChrome <shandkat3@gmail.com>
2020-04-17 16:37:00 +0300

    add xml comments and AppVeyor config

--- test/test1/Queue/Queue.cs
+++ test/test1/Queue/Queue.cs
@@ -1,13 +1,32 @@
 using System;
 using System.Collections;
 
+/// <summary>
+/// пространство имен очередь
+/// </summary>
 namespace Queue
 {
+    /// <summary>
+    /// Класс , реализующий элемент очереди
+    /// </summary>
+    /// <typeparam name="T">тип</typeparam>
     public class QueueElement<T>
     {
+        /// <summary>
+        /// приоритет элемента
+        /// </summary>
+        /// <value>значение приоритета</value>
         public int Priority { get; set; }
+
+        /// <summary>
+        /// Данные элемента
+        /// </summary>
+        /// <value>данные</value>
         public T Data { get; set; }
 
+        /// <summary>
+        /// Конструктор элемента, принимающий данные и приоритет элемента
+        /// </summary>
         public QueueElement(T data, int priority)
         {
             Data = data;
@@ -15,22 +34,39 @@ namespace Queue
         }
     }
 
+    /// <summary>
+    /// Класс, реализующий очередь
+    /// </summary>
+    /// <typeparam name="T">тип элементов очереди</typeparam>
     public class Queue<T>
     {
+        /// <summary>
+        /// Массив элементов
+        /// </summary>
         private QueueElement<T>[] QueueElements;
 
+        /// <summary>
+        /// Конструктор очереди, принимающий данные и приоритет превого элемента
+        /// </summary>
         public Queue(T data, int priority)
         {
             QueueElements = new QueueElement<T>[1];
             QueueElements[0] = new QueueElement<T>(data, priority);
         }
 
+        /// <summary>
+        /// Метод добавления элемента в очередь
+        /// </summary>
         public void Enqueue(T data, int priority)
         {
             Array.Resize(ref QueueElements, QueueElements.Length + 1);
             QueueElements[QueueElements.Length - 1] = new QueueElement<T>(data, priority);
         }
 
+        /// <summary>
+        /// Поиск максимального приоритета
+        /// </summary>
+        /// <returns>максимальный приоритет</returns>
         private int FindMaximumPriority()
         {
             int maximumPriority = QueueElements[0].Priority;
@@ -46,6 +82,9 @@ namespace Queue
             return maximumPriority;
         }
 
+        /// <summary>
+        /// Поиск индекса первого элемента с максимальным приоритетом
+        /// </summary>
         private int GetIndexOfTheFirstElementWithMaximumPriority(int maximumPriority)
         {
             int index = 0;
@@ -60,6 +99,9 @@ namespace Queue
             return index;
         }
 
+        /// <summary>
+        /// Удаление элемента по заданному индексу и возвращение его данных
+        /// </summary>
         private T GetDataOfElementAndRemoveThem(int index)
         {
             T data = QueueElements[index].Data;
@@ -73,6 +115,9 @@ namespace Queue
             return data;
         }
 
+        /// <summary>
+        /// Удаление элемента с наивысшим приоритетом, вошедшего раньше всех
+        /// </summary>
         public T Dequeue()
         {
             if (QueueElements.Length == 0)
