884035d4da3cb3847c8a55966f95b051fd492005
kat3chrome <shandkat3@gmail.com>
2020-05-07 16:15:41 +0600

    hello world
    Create new project

---
+++ hw4/hw4.2/UniqueList/UniqueList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UniqueList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
