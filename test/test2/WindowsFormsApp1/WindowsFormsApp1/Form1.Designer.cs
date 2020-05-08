73b5fe1f39c3125e1a54b5c18b31300dbb3e5e53
kat3chrome <shandkat3@gmail.com>
2020-05-08 16:40:45 +0600

    remove hidden button click action

--- test/test2/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ test/test2/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -57,7 +57,6 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             this.button2.MouseEnter += new System.EventHandler(this.button2_MouseEnter);
             // 
             // Form1
