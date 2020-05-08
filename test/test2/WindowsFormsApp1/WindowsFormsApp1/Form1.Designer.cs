5156f3d2a3955bb41008576913e30a73b1df3402
kat3chrome <shandkat3@gmail.com>
2020-05-08 16:33:01 +0600

    ready programm
    add xml comments

--- test/test2/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ test/test2/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.Lime;
-            this.button1.Location = new System.Drawing.Point(278, 276);
+            this.button1.Location = new System.Drawing.Point(159, 163);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(47, 37);
             this.button1.TabIndex = 0;
@@ -51,9 +51,9 @@
             this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.ForeColor = System.Drawing.Color.Black;
-            this.button2.Location = new System.Drawing.Point(197, 194);
+            this.button2.Location = new System.Drawing.Point(138, 147);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(200, 200);
+            this.button2.Size = new System.Drawing.Size(89, 70);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = false;
@@ -63,13 +63,14 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(684, 661);
+            this.ClientSize = new System.Drawing.Size(376, 369);
+            this.ControlBox = false;
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button2);
             this.ForeColor = System.Drawing.Color.Black;
             this.Name = "Form1";
-            this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
--- test/test2/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ test/test2/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -12,34 +12,49 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Form1"/> class.
+        /// </summary>
         public Form1()
         {
+            this.MinimumSize = new Size(400, 400);
+            this.MaximumSize = new Size(400, 400);
             InitializeComponent();
         }
-
+        /// <summary>
+        /// Handles the Load event of the Form1 control.
+        /// </summary>
         private void Form1_Load(object sender, EventArgs e)
         {
         }
-
+        /// <summary>
+        /// Handles the Click event of the button1 control.
+        /// </summary>
         private void button1_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
-
+        /// <summary>
+        /// Handles the 1 event of the button1_MouseLeave control. Changes color when removed mouse.
+        /// </summary>
         private void button1_MouseLeave_1(object sender, EventArgs e)
         {
             this.button1.BackColor = Color.Lime;
         }
-
+        /// <summary>
+        /// Handles the MouseEnter event of the button1 control. Changes color when removed mouse.
+        /// </summary>
         private void button1_MouseEnter(object sender, EventArgs e)
         {
             this.button1.BackColor = Color.Red;
         }
-
+        /// <summary>
+        /// Handles the MouseEnter event of the button2 control. Сhanges position when hovering mouse.
+        /// </summary>
         private void button2_MouseEnter(object sender, EventArgs e)
         {
-            this.button1.Location = new System.Drawing.Point(Cursor.Position.X % this.Size.Height, Cursor.Position.Y % this.Size.Height);
-            this.button2.Location = new System.Drawing.Point(Cursor.Position.X % this.Size.Height, Cursor.Position.Y % this.Size.Height);
+            this.button1.Location = new System.Drawing.Point((Control.MousePosition.X + 1000) % 300 , (Control.MousePosition.Y + 1000) % 300);
+            this.button2.Location = new System.Drawing.Point((Control.MousePosition.X + 1000) % 300, (Control.MousePosition.Y + 1000) % 300);
         }
     }
 }
