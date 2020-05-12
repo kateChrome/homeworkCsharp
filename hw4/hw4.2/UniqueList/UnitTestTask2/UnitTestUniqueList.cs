57fa42d32e81a9b8e8ee5ec4a445fb456a9bcbd1
kat3chrome <shandkat3@gmail.com>
2020-05-12 18:37:32 +0600

    Add test

---
+++ hw4/hw4.2/UniqueList/UnitTestTask2/UnitTestUniqueList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task2;
+
+namespace UnitTestTask2
+{
+    [TestClass]
+    public class UnitTestUniqueList
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ElementAlreadyExistException))]
+        public void AddOnlyUniqueItemsTest()
+        {
+            var list = new UniqueList<string>();
+            list.Add("1");
+            list.Add("1");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ElementAlreadyExistException))]
+        public void AddUniqueItemsTest()
+        {
+            var list = new UniqueList<string>();
+            list.Add("1");
+            list.Add("2");
+            list.Add("1");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ElementAlreadyExistException))]
+        public void InsertOnlyUniqueItemsTest()
+        {
+            var list = new UniqueList<string>();
+            list.Insert(0,"1");
+            list.Insert(1,"1");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ElementAlreadyExistException))]
+        public void InsertUniqueItemsTest()
+        {
+            var list = new UniqueList<string>();
+            list.Insert(0,"1");
+            list.Insert(1,"2");
+            list.Insert(2,"1");
+        }
+    }
+}
