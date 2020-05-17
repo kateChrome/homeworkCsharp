ff1f3bfa8697f6a423f4d986ba032fedad051780
kat3chrome <shandkat3@gmail.com>
2020-05-17 19:02:12 +0600

    create new project

---
+++ hw7/hw7.2/Clock/Clock/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class Form1 : Form
+    {
+        private static DateTime _dateTime = DateTime.Now;
+        int hour = _dateTime.Hour % 12;
+        int minute = _dateTime.Minute;
+        int secund = _dateTime.Second;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
