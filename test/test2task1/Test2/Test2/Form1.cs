9b9e1d5869481b11ab59f7077d2020408a1b63d0
kat3chrome <shandkat3@gmail.com>
2020-05-22 16:49:19 +0600

    ready programm

--- test/test2task1/Test2/Test2/Form1.cs
+++ test/test2task1/Test2/Test2/Form1.cs
@@ -12,9 +12,100 @@ namespace Test2
 {
     public partial class Form1 : Form
     {
+        private int inputParameter = 2;
+        private int buttonSize = 50;
+        private int font = 10;
+        private int sleepTime = 500;
+        private int numberOfDisabledButtons = 0;
+
+        private bool pressedButtonExists = false;
+        private Button[] pressedButtons;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private List<int> CreatePayload()
+        {
+            var payload = new List<int>();
+            for (var i = 0; i < inputParameter * inputParameter / 2; i++)
+            {
+                payload.Add(i);
+                payload.Add(i);
+            }
+
+            return payload;
+        }
+
+        private void GameFormLoad(object sender, EventArgs e)
+        {
+            this.Size = new Size((inputParameter + 1) * buttonSize, (inputParameter + 2) * buttonSize);
+            pressedButtons = new Button[2];
+
+            var payload = CreatePayload();
+            for (var i = 0; i < inputParameter; i++)
+            {
+                for (var j = 0; j < inputParameter; j++)
+                {
+                    var button = new Button
+                    {
+                        Location = new Point(i * buttonSize, j * buttonSize), 
+                        Size = new Size(buttonSize, buttonSize),
+                        Font = new Font("Minion Pro", font), 
+                        Padding = new Padding(0)
+                    };
+
+                    var random = new Random().Next(payload.Count - 1);
+                    button.Name = payload[random].ToString();
+                    payload.RemoveAt(random);
+
+                    button.Click += new EventHandler(DynamicButton_Click);
+
+                    this.Controls.Add(button);
+                }
+            }
+        }
+
+        private void DynamicButton_Click(object sender, EventArgs e)
+        {
+            var clickedButton = (Button) sender;
+            clickedButton.Enabled = false;
+            clickedButton.Text = clickedButton.Name;
+
+            if (!pressedButtonExists)
+            {
+                pressedButtonExists = true;
+                pressedButtons[0] = clickedButton;
+            }
+            else
+            {
+                pressedButtons[1] = clickedButton;
+                ButtonComparison();
+            }
+        }
+
+        private void ButtonComparison()
+        {
+            pressedButtonExists = false;
+
+            if (pressedButtons[0].Name != pressedButtons[1].Name)
+            {
+                Task.Delay(sleepTime).Wait();
+                pressedButtons[0].Text = "";
+                pressedButtons[1].Text = "";
+                pressedButtons[0].Enabled = true;
+                pressedButtons[1].Enabled = true;
+            }
+            else
+            {
+                numberOfDisabledButtons += 2;
+
+                if (numberOfDisabledButtons == this.Controls.Count)
+                {
+                    Application.Exit();
+                }
+            }
         }
     }
 }
